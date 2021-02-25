d0e5382b827e33009084f8a7257a46820fd8a7ad
ezzygit <ezzaemails@gmail.com>
2021-02-25 09:24:05 +0000

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {commerce} from './lib/commerce';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from './components/pages/Home';
 import Shop from './components/pages/Shop';
+import Footer from './components/Footer';
+
+
 
 const App = () => {
+
+const [ products, setProducts] = useState([]);
+
+const fetchProducts = async () => {
+  const response = await commerce.products.list();
+
+  setProducts();
+}
+
+useEffect(() => {
+  fetchProducts();
+})
+
+
     return (
       <div className="App">
         <Router>
@@ -13,13 +31,15 @@ const App = () => {
             <Navbar />
             <Switch>
           <Route path='/' exact component={Home}  />
-          <Route path='/Shop' exact component={Shop}  />
+          <Route path='/Shop' exact component={Shop}/>
           
           
           
           
         </Switch>
           </div>
+        
+        <Footer/>
         </Router>
       </div>
     );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,13 @@
 import '../../App.css'
 import MainCaro from '../MainCaro'
 import React from 'react';
-import ImageSlider from '../ImageSlider';
-import InstagramEmbed from 'react-instagram-embed';
+
 
 function Home() {
     return (
         <div>
             <MainCaro/>
-            <ImageSlider/>
+            
         </div>
     )
 }
